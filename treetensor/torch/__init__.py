a5a6704c8b7c88a49c73b8d710206813a0f969be
HansBug <killog@126.com>
2021-09-29 00:11:03 +0800

    dev(hansbug): make treetensor.torch automatic

--- treetensor/torch/__init__.py
+++ treetensor/torch/__init__.py
@@ -1,12 +1,63 @@
+import builtins
+from types import ModuleType, FunctionType, BuiltinFunctionType
+from typing import Iterable
+
+import torch
+
 from .funcs import *
 from .funcs import __all__ as _funcs_all
+from .funcs.base import get_func_from_torch
 from .size import *
 from .size import __all__ as _size_all
 from .tensor import *
 from .tensor import __all__ as _tensor_all
+from ..config.meta import __VERSION__
 
 __all__ = [
     *_funcs_all,
     *_size_all,
     *_tensor_all,
 ]
+
+_basic_types = (
+    builtins.bool, builtins.bytearray, builtins.bytes, builtins.complex, builtins.dict,
+    builtins.float, builtins.frozenset, builtins.int, builtins.list, builtins.range, builtins.set,
+    builtins.slice, builtins.str, builtins.tuple,
+)
+_torch_all = set(torch.__all__)
+
+
+class _Module(ModuleType):
+    def __init__(self, module):
+        ModuleType.__init__(self, module.__name__)
+
+        for name in filter(lambda x: x.startswith('__') and x.endswith('__'), dir(module)):
+            setattr(self, name, getattr(module, name))
+        self.__origin__ = module
+        self.__torch_version__ = torch.__version__
+        self.__version__ = __VERSION__
+
+    def __getattr__(self, name):
+        if (name in self.__all__) or \
+                (hasattr(self.__origin__, name) and isinstance(getattr(self.__origin__, name), ModuleType)):
+            return getattr(self.__origin__, name)
+        else:
+            item = getattr(torch, name)
+            if isinstance(item, (FunctionType, BuiltinFunctionType)) and not name.startswith('_'):
+                return get_func_from_torch(name)
+            elif (isinstance(item, torch.dtype)) or \
+                    isinstance(item, _basic_types) and name in _torch_all:
+                return item
+            else:
+                raise AttributeError(f'Attribute {repr(name)} not found in {repr(__name__)}.')
+
+    def __hasattr__(self, name):
+        return name in self.__all__
+
+    def __dir__(self) -> Iterable[str]:
+        return self.__all__
+
+
+import sys
+
+sys.modules[__name__] = _Module(sys.modules[__name__])
--- treetensor/torch/funcs/base.py
+++ treetensor/torch/funcs/base.py
@@ -1,3 +1,5 @@
+from functools import wraps
+
 import torch
 from treevalue import TreeValue
 from treevalue import func_treelize as original_func_treelize
@@ -6,6 +8,7 @@ from treevalue.utils import post_process
 
 from ..base import auto_torch
 from ..tensor import Tensor
+from ...common import return_self
 from ...utils import doc_from_base as original_doc_from_base
 from ...utils import replaceable_partial, args_mapping
 
@@ -15,3 +18,18 @@ func_treelize = post_process(post_process(args_mapping(
 )
 doc_from_base = replaceable_partial(original_doc_from_base, base=torch)
 auto_tensor = replaceable_partial(auto_torch, cls=Tensor)
+
+
+def get_func_from_torch(name):
+    func = getattr(torch, name)
+
+    @func_treelize()
+    @wraps(func)
+    def _new_func(*args, **kwargs):
+        return func(*args, **kwargs)
+
+    if func.__name__.endswith("_"):
+        _new_func = return_self(_new_func)
+    _new_func = doc_from_base()(_new_func)
+
+    return _new_func
