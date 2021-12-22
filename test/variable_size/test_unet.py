dfe1472730aca2008c2f2476230b49d14ec3495a
niuyazhe <niuyazhe@sensetime.com>
2021-12-22 20:51:19 +0800

    demo(nyz): add padding unet demo

--- test/variable_size/test_unet.py
+++ test/variable_size/test_unet.py
@@ -34,6 +34,36 @@ def naive_method(model, data, cuda, test_loop=3):
     return output, result
 
 
+def padding_method(model, data, cuda, test_loop=3):
+    result = []
+    for _ in range(test_loop):
+        t_start = time.time()
+        with torch.no_grad():
+            # padding
+            max_h = max([d.shape[-2] for d in data])
+            max_w = max([d.shape[-1] for d in data])
+            new_data = torch.zeros(len(data), 3, max_h, max_w).to(data[0].device)
+            start_h = [max_h - d.shape[-2] for d in data]
+            start_w = [max_w - d.shape[-1] for d in data]
+            for i in range(len(data)):
+                new_data[i, :, start_h[i]:, start_w[i]:] = data[i]
+
+            padding_output = model(new_data)
+
+            output = []
+            for i in range(len(data)):
+                output.append(
+                    padding_output[i, :, start_h[i]:start_h[i] + data[i].shape[-2],
+                                   start_w[i]:start_w[i] + data[i].shape[-1]]
+                )
+        if cuda:
+            torch.cuda.synchronize()
+        t_end = time.time()
+        result.append(t_end - t_start)
+    print('padding_method test time avg: {}, max: {}'.format(np.mean(result), np.max(result)))
+    return output, result
+
+
 def nestedtensor_method(model, data, cuda, test_loop=3):
     result = []
     data = nestedtensor.nested_tensor([d.squeeze(0) for d in data])
@@ -74,7 +104,8 @@ def main(cuda):
 
     same_output, same_result = same_test(model, data, cuda)
     naive_output, naive_result = naive_method(model, data, cuda)
-    assert len(naive_output) == B
+    assert len(naive_output) == B, len(naive_output)
+    padding_output, padding_result = padding_method(model, data, cuda)
     nest_output, nest_result = nestedtensor_method(model, data, cuda)
     print(naive_output[0][0, 0, 0, :10])
     print(nest_output[0][0, 0, 0, :10])
