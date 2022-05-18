e5fcb36759a0060f30e4e518e0ea0887c70d41a8
HansBug <hansbug@buaa.edu.cn>
2022-05-19 00:04:22 +0800

    dev(hansbug): update runs script

--- runs/test_plain.py
+++ runs/test_plain.py
@@ -1,14 +1,13 @@
 import torch
 
-if __name__ == '__main__':
-    # create a native tensor
-    t = torch.randn((6, 2, 3))
+# create a native tensor
+t = torch.randn((6, 2, 3))
 
-    # structural operation
-    print(torch.stack([t, t, t]))
-    print(torch.cat([t, t, t]))
-    print(torch.split(t, (1, 2, 3)))
+# structural operation
+print(torch.stack([t, t, t]))
+print(torch.cat([t, t, t]))
+print(torch.split(t, (1, 2, 3)))
 
-    # math calculations
-    print(t ** 2)
-    print(torch.sin(t).cos())
+# math calculations
+print(t ** 2)
+print(torch.sin(t).cos())
