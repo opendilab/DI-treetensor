e9612e6a218a0877ea334965ab2ec3971f231e82
HansBug <hansbug@buaa.edu.cn>
2024-10-21 15:53:13 +0800

    dev(hansbug): use version v0.5.0

--- treetensor/config/meta.py
+++ treetensor/config/meta.py
@@ -7,7 +7,7 @@ Overview:
 __TITLE__ = "DI-treetensor"
 
 #: Version of this project.
-__VERSION__ = "0.4.1"
+__VERSION__ = "0.5.0"
 
 #: Short description of the project, will be included in ``setup.py``.
 __DESCRIPTION__ = 'A flexible, generalized tree-based tensor structure.'
